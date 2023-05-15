bada855e2eafbab1b6c20f06b05db4fd3de7abc3
Trusova <kace-oksana@yandex.ru>
2023-05-16 01:24:46 +0300

    new

---
+++ src/Collection/CreateCollect.java
@@ -0,0 +1,20 @@
+package Collection;
+
+import java.io.OptionalDataException;
+import java.util.ArrayList;
+
+import static sun.security.util.Debug.args;
+
+public class CreateCollect {
+    public CreateCollect() {
+
+    public static void main(Object [] args) {
+        Coll C1 =new ArrayList();
+
+    }
+    int i  = 0;
+        OptionalDataException arrayList;
+        for (int i; i < arrayList.length; i++){
+            if
+        }
+}
---
+++ src/Collection/Service.java
@@ -0,0 +1,35 @@
+package Collection;
+
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Service extends CreateCollect {
+    public static void main(String[] args) {
+        Collection<Integer> arrlist = new ArrayList<Integer>(5);
+    }
+
+    public void add(E) {
+        return add;
+
+
+        ArrayList<String> list;
+
+        Object arrList = null;
+        for (
+                int i = arrList.lenght() - 1;
+                i >= 0; --i) {
+            JavaIOFileDescriptorAccess arrlist = null;
+            if (arrlist.get(i).contains()) {
+                list.remove(i);
+            }
+        }
+
+
+    }
+
+
+}
+
